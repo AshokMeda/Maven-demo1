07eb9585b4bfabd882f5031abab10afe923a99ef
Ashok <ashokmeda54321@gmail.com>
2024-06-01 16:12:04 +0530

    intial commit for maven

---
+++ src/main/java/com/vedit/devp73/maven_demo1/App.java
@@ -0,0 +1,13 @@
+package com.vedit.devp73.maven_demo1;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
